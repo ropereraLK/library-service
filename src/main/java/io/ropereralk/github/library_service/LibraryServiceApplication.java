149792479d73fa51765041a1c83afa09f4e7d439
Rohan Perera <ropereradev@gmail.com>
2026-06-18 12:00:57 +0530

    Created springboot project

---
+++ src/main/java/io/ropereralk/github/library_service/LibraryServiceApplication.java
@@ -0,0 +1,13 @@
+package io.ropereralk.github.library_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryServiceApplication.class, args);
+	}
+
+}
